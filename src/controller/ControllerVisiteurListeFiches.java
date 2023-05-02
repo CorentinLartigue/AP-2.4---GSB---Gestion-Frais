829ee966c940c9f60779dd6b854a1458db2d9723
Joseph LARRIEU FRANCOIS <jlarrieu@geiffel.fr>
2023-05-02 15:52:52 +0200

    btn-creer

--- src/controller/ControllerVisiteurListeFiches.java
+++ src/controller/ControllerVisiteurListeFiches.java
@@ -1,17 +1,24 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+
+import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.DAO.FicheFraisDAO;
 import model.DAO.UtilisateurDAO;
@@ -139,7 +146,20 @@ public class ControllerVisiteurListeFiches{
 
 			
 	 } 
-	 
+	 @FXML protected void onbuttonCréerFicheVisiteurClik(ActionEvent e) throws IOException{ 
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/viewCreationFicheFrais.fxml"));
+		Pane visiteurcreerFichesLayout = (Pane) loader.load();
+	 	Scene visiteurcreerFichesScene = new Scene(visiteurcreerFichesLayout);
+     	Stage visiteurcreerFichesStage = new Stage();
+     	visiteurcreerFichesStage.setScene(visiteurcreerFichesScene);
+    		
+     	visiteurcreerFichesStage.setTitle("GSB Gestion des frais - Visiteurs création Fiches de frais");
+     	visiteurcreerFichesStage.initModality(Modality.APPLICATION_MODAL);		 		
+     	visiteurcreerFichesStage.show();
+
+
+	 }
 
 
 
