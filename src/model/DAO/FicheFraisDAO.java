5c60fa66573c3b72ab9b84d02682d6f5a77a9f45
Romain CASSOULET <rcassoulet@geiffel.fr>
2023-05-09 15:49:50 +0200

    FINALE

---
+++ src/model/DAO/FicheFraisDAO.java
@@ -0,0 +1,69 @@
+package model.DAO;
+
+
+import java.sql.ResultSet;
+public class FicheFraisDAO {
+	
+	/**
+	 * M�thode permettant de r�cup�rer les fiches de frais
+	 * @return ResultSet (la liste des fiches de frais
+	 */
+	 public static ResultSet lesFichesFrais()
+     {
+		 		 
+	     String requete = "SELECT *  FROM fichefrais order by annee desc , mois desc";
+                 
+         return DBConnex.getRS(requete, DBConnex.connexion());
+ 		
+ 		
+     }
+	 
+
+	 
+	 /**
+	  * M�thode permettant de r�cup�rer les lignes d'une fiche de frais
+	  * @param unIdFiche
+	  * @return ResultSet (la liste des lignes d'une fiche de frais
+	  */
+	 public static ResultSet lesLignesFicheFrais(int unIdFiche)
+     {
+		 		 
+	     String requete = "SELECT lignefrais.*, libelle , montant  FROM lignefrais join typefrais on lignefrais.idTypeFrais = typefrais.id  where idFiche = " + unIdFiche ;
+                 
+         return DBConnex.getRS(requete, DBConnex.connexion());
+ 		
+     }
+	
+	 
+	 /**
+	  * M�thode permettant de modifier l'�tat d'une fiche de frais
+	  * @param unIdFiche
+	  * @param nouvelEtat (code �tat)
+	  * @return Interger 
+	  */
+	 public static Integer changerEtat(int unIdFiche , String nouvelEtat)
+     {
+		 		 
+	     String requete = "update ficheFrais set idEtat = '" + nouvelEtat + "' where idFiche = " + unIdFiche ;
+                 
+         return DBConnex.noQuery(requete, DBConnex.connexion());
+ 		
+ 		
+     }
+	 
+	 
+	 public static Integer changerQuantiteValidee(int unIdFiche , int quantite , String typeFrais)
+     {
+		 		 
+	     String requete = "update lignefrais set quantiteValidee = " + quantite + " where idFiche = " + unIdFiche + " and idTypeFrais ='"  + typeFrais+ "'";
+                 
+	     System.out.println(requete);
+	     
+         return DBConnex.noQuery(requete, DBConnex.connexion());
+ 		
+ 		
+     }
+	 
+	 
+	 
+}
